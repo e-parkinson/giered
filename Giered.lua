92f4283dbf61ba59c84bcf7674677e139769fb68
Esme Parkinson <parkinson.esme@gmail.com>
2016-11-01 13:58:28 +0000

    Adding class identificatio before greeting

--- Giered.lua
+++ Giered.lua
@@ -1,5 +1,6 @@
 local EventFrame = CreateFrame("Frame")
 EventFrame:RegisterEvent("PLAYER_LOGIN")
+local playerClass, englishClass = UnitClass("player");
 EventFrame:SetScript("OnEvent", function(self,event,...)
-	ChatFrame1:AddMessage('Giered is running for '..UnitName("Player"))
+	ChatFrame1:AddMessage('Giered is running for '..UnitName("Player") ..' '.. englishClass)
 end)
