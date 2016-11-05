cea57b620b6987d79ead847f3fd8290af1154138
Esme Parkinson <parkinson.esme@gmail.com>
2016-11-05 21:32:09 +0000

    changing message displayed on login

--- Giered.lua
+++ Giered.lua
@@ -3,14 +3,13 @@ local LogInFrame = CreateFrame("Frame")
 LogInFrame:RegisterEvent("PLAYER_LOGIN")
 local playerClass, englishClass = UnitClass("player")
 LogInFrame:SetScript("OnEvent", function(self,event,...)
-	ChatFrame1:AddMessage('Giered is running for '..UnitName("Player") ..' - '..playerClass)
+	ChatFrame1:AddMessage('Giered läuft jetzt für '..UnitName("Player") ..' ('..playerClass ..')')
 end)
 --perform on loot roll
 local LootRollFrame = CreateFrame("Frame")
 LootRollFrame:RegisterEvent("START_LOOT_ROLL")
 LootRollFrame:SetScript("OnEvent", function(self, event, id)
 	--get loot id and use to find name
-	--local lootID, lootTimer = ...
 	local texture, lootName, count, quality, bindOnPickUp = GetLootRollItemInfo(id)
 	ChatFrame1:AddMessage('NEW LOOT ROLL ' ..lootName)
 end)
